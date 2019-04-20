2e237afcdb1d4cebb25a3a30077d0cebb4e191d4
J. Singleton <jennifer.singleton@gmail.com>
2019-04-20 21:50:40 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,7 +18,7 @@ class ApplicationController < Sinatra::Base
 
   post "/signup" do
     #your code here
-    put params
+    puts params
     user = User.new(username: params[:username], password: params[:password])
 
     if user
